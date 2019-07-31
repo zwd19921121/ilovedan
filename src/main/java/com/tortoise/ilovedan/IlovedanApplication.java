eea33f09661a68b028049b2cdcd96440a82bfcfb
zwd19921121 <1044755836@qq.com>
2019-07-31 20:58:15 +0800

    初始化

---
+++ src/main/java/com/tortoise/ilovedan/IlovedanApplication.java
@@ -0,0 +1,13 @@
+package com.tortoise.ilovedan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlovedanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IlovedanApplication.class, args);
+    }
+
+}
